1faaae23e9f65d9f771e7c6f9a25f727608ace90
Calham Jackson Northway <39806672+CalhamJNorthway@users.noreply.github.com>
2021-04-12 16:13:02 -0700

    Try tings

--- server.js
+++ server.js
@@ -5,28 +5,27 @@ import { Server, Socket } from "socket.io";
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
-
-let userCount = 0;
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket: Socket) => {
-  socket.on('userConnection', (userName: string) => {
+io.on('connection', (socket) => {
+  socket.on('userConnection', (userName) => {
     console.log('user: ' + userName + ' has connected.');
     io.emit('connectionChange', { userName, event: 'entry' });
   })
 
-  socket.on('chat message', (request: { value: string, user: string }) => {
+  socket.on('chat message', (request) => {
     io.emit('chat message', request);
   });
 
-  socket.on('userDisconnect', (userName: string) => {
+  socket.on('userDisconnect', (userName) => {
     console.log('user disconnected');
     io.emit('connectionChange', { userName, event: 'exit' });
   })
 });
-server.listen(8080, () => {
+server.listen(PORT, () => {
   console.log('listening on PORT:8080');
 });
